58a01fb0b5e6a5ed8d625609ab9296d6dfb03eba
Ahmed Bashir <ahmedbashir.w@gmail.com>
2025-03-20 03:55:14 +0500

    Made Entry component reusable using Props

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,21 +1,21 @@
 import marker from "../assets/marker.png";
 
-export default function Entry(){
+export default function Entry({img, title, country, googleMapsLink, dates, text}){
     return(
         <div className="journal-entry">
             <div className="main-image-container">
             <img 
                 className="thumbnail"
-                src="https://scrimba.com/links/travel-journal-japan-image-url" 
-                alt="Mount Fuji" />
+                src={img.src} 
+                alt={img.alt} />
             </div>
             <div className="info-container">
                 <img className="map-marker" src={marker} alt="Marker Icon" />
-                <span>JAPAN</span>
-                <a href="#">View on Google Maps</a>
-                <h2>Mount Fuji</h2>
-                <span className="trip-date">12 Jan, 2023 - 24 Jan, 2023</span>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <span>{country}</span>
+                <a href={googleMapsLink}>View on Google Maps</a>
+                <h2>{title}</h2>
+                <span className="trip-date">{dates}</span>
+                <p>{text}</p>
             </div>
         </div>
     )
